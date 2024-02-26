f114476ea25203e865aea2369795b5f487004fa0
BeardedBeaver <BeardedBeaver@users.noreply.github.com>
2024-02-26 17:54:28 +0000

    deploy: 07a1f3d452910022b17fe47344808075118da22a

--- menudata.js
+++ menudata.js
@@ -59,7 +59,7 @@ var menudata={children:[
 {text:"u",url:"functions_func.html#index_u"},
 {text:"y",url:"functions_func.html#index_y"}]},
 {text:"Variables",url:"functions_vars.html"},
-{text:"Related Functions",url:"functions_rela.html"}]}]},
+{text:"Related Symbols",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
