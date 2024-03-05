d5305953928bcd1f92b918f53e136d2d4c25beb0
BeardedBeaver <BeardedBeaver@users.noreply.github.com>
2024-03-05 22:47:20 +0000

    deploy: b7219553c3038b2ead91e4e0c2e9d8f38d16e99b

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['qaliasdictionary_0',['qaliasdictionary',['../class_q_alias_dictionary.html#a57685c66e6998b75ec3658ea506ec5a5',1,'QAliasDictionary::QAliasDictionary()'],['../class_q_alias_dictionary.html',1,'QAliasDictionary&lt; String &gt;']]],
+  ['qaliasdictionary_0',['QAliasDictionary',['../class_q_alias_dictionary.html',1,'']]],
   ['qaliasdictionary_2eh_1',['qaliasdictionary.h',['../qaliasdictionary_8h.html',1,'']]],
   ['qaliasdictionary_3c_20qstring_20_3e_2',['QAliasDictionary&lt; QString &gt;',['../class_q_alias_dictionary.html',1,'']]],
   ['qaliasdictionarytests_3',['qaliasdictionarytests',['../class_q_alias_dictionary_tests.html',1,'QAliasDictionaryTests'],['../class_q_alias_dictionary_tests.html#a1fa0ae347f2401b569ffe9aeeb30e9a4',1,'QAliasDictionaryTests::QAliasDictionaryTests()']]],
@@ -13,16 +13,16 @@ var searchData=
   ['qlinearfunctiontests_2ecpp_10',['qlinearfunctiontests.cpp',['../qlinearfunctiontests_8cpp.html',1,'']]],
   ['qlinearfunctiontests_2eh_11',['qlinearfunctiontests.h',['../qlinearfunctiontests_8h.html',1,'']]],
   ['qunitconversion_12',['QUnitConversion',['../index.html',1,'']]],
-  ['qunitconversionfamily_13',['qunitconversionfamily',['../class_q_unit_conversion_family.html#a878dbd0340bf8d5d514e205e1ce08dc9',1,'QUnitConversionFamily::QUnitConversionFamily(const QString &amp;familyName, const QString &amp;baseUnit)'],['../class_q_unit_conversion_family.html#a7fbf3e754d7cf7d9e11bead3952e2ed9',1,'QUnitConversionFamily::QUnitConversionFamily()=default'],['../class_q_unit_conversion_family.html',1,'QUnitConversionFamily']]],
+  ['qunitconversionfamily_13',['qunitconversionfamily',['../class_q_unit_conversion_family.html',1,'QUnitConversionFamily'],['../class_q_unit_conversion_family.html#a7fbf3e754d7cf7d9e11bead3952e2ed9',1,'QUnitConversionFamily::QUnitConversionFamily()=default'],['../class_q_unit_conversion_family.html#a878dbd0340bf8d5d514e205e1ce08dc9',1,'QUnitConversionFamily::QUnitConversionFamily(const QString &amp;familyName, const QString &amp;baseUnit)']]],
   ['qunitconversionfamily_2ecpp_14',['qunitconversionfamily.cpp',['../qunitconversionfamily_8cpp.html',1,'']]],
   ['qunitconversionfamily_2eh_15',['qunitconversionfamily.h',['../qunitconversionfamily_8h.html',1,'']]],
-  ['qunitconversionrule_16',['qunitconversionrule',['../class_q_unit_conversion_rule.html',1,'QUnitConversionRule'],['../class_q_unit_conversion_rule.html#a7d1600fc4370c7ed33087eab7ed0df8f',1,'QUnitConversionRule::QUnitConversionRule(const QString &amp;family, const QString &amp;baseUnit, const QString &amp;unit, const QLinearFunction &amp;convertFunction)'],['../class_q_unit_conversion_rule.html#a740ceebd3a3e74c1fe383a7b6e33811e',1,'QUnitConversionRule::QUnitConversionRule()=default'],['../class_q_unit_conversion_rule.html#ab4a8729e611b00db2cdddde548e71673',1,'QUnitConversionRule::QUnitConversionRule(const QString &amp;family, const QString &amp;baseUnit, const QString &amp;unit, double k, double b)']]],
+  ['qunitconversionrule_16',['qunitconversionrule',['../class_q_unit_conversion_rule.html',1,'QUnitConversionRule'],['../class_q_unit_conversion_rule.html#ab4a8729e611b00db2cdddde548e71673',1,'QUnitConversionRule::QUnitConversionRule(const QString &amp;family, const QString &amp;baseUnit, const QString &amp;unit, double k, double b)'],['../class_q_unit_conversion_rule.html#a7d1600fc4370c7ed33087eab7ed0df8f',1,'QUnitConversionRule::QUnitConversionRule(const QString &amp;family, const QString &amp;baseUnit, const QString &amp;unit, const QLinearFunction &amp;convertFunction)'],['../class_q_unit_conversion_rule.html#a740ceebd3a3e74c1fe383a7b6e33811e',1,'QUnitConversionRule::QUnitConversionRule()=default']]],
   ['qunitconversionrule_2ecpp_17',['qunitconversionrule.cpp',['../qunitconversionrule_8cpp.html',1,'']]],
   ['qunitconversionrule_2eh_18',['qunitconversionrule.h',['../qunitconversionrule_8h.html',1,'']]],
-  ['qunitconvertor_19',['qunitconvertor',['../class_q_unit_convertor.html#ae1b74bed8a9b7330c0c72fa7bcefe90d',1,'QUnitConvertor::QUnitConvertor()'],['../class_q_unit_convertor.html',1,'QUnitConvertor']]],
+  ['qunitconvertor_19',['qunitconvertor',['../class_q_unit_convertor.html',1,'QUnitConvertor'],['../class_q_unit_convertor.html#ae1b74bed8a9b7330c0c72fa7bcefe90d',1,'QUnitConvertor::QUnitConvertor()']]],
   ['qunitconvertor_2ecpp_20',['qunitconvertor.cpp',['../qunitconvertor_8cpp.html',1,'']]],
   ['qunitconvertor_2eh_21',['qunitconvertor.h',['../qunitconvertor_8h.html',1,'']]],
-  ['qunitconvertortests_22',['qunitconvertortests',['../class_q_unit_convertor.html#a1f407847a5b78799e02a4982d0340aa3',1,'QUnitConvertor::QUnitConvertorTests'],['../class_q_unit_convertor_tests.html#a21a5d46e503af20955b8fa1ac3317191',1,'QUnitConvertorTests::QUnitConvertorTests()'],['../class_q_unit_convertor_tests.html',1,'QUnitConvertorTests']]],
+  ['qunitconvertortests_22',['qunitconvertortests',['../class_q_unit_convertor_tests.html',1,'QUnitConvertorTests'],['../class_q_unit_convertor.html#a1f407847a5b78799e02a4982d0340aa3',1,'QUnitConvertor::QUnitConvertorTests'],['../class_q_unit_convertor_tests.html#a21a5d46e503af20955b8fa1ac3317191',1,'QUnitConvertorTests::QUnitConvertorTests()']]],
   ['qunitconvertortests_2ecpp_23',['qunitconvertortests.cpp',['../qunitconvertortests_8cpp.html',1,'']]],
   ['qunitconvertortests_2eh_24',['qunitconvertortests.h',['../qunitconvertortests_8h.html',1,'']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['addalias_0',['addAlias',['../class_q_alias_dictionary.html#a61264607573e4d2fb0f2a0f95d8bc6ab',1,'QAliasDictionary']]],
+  ['addalias_0',['addalias',['../class_q_alias_dictionary.html#a7645ae8ba63b123bced892bf17009fa6',1,'QAliasDictionary::addAlias()'],['../class_q_unit_convertor.html#a4afbaf7b8e2313965480250157e2dbcb',1,'QUnitConvertor::addAlias()']]],
   ['addconversionrule_1',['addconversionrule',['../class_q_unit_conversion_family.html#a8aceb964519aad7447795ccb28d5e2ec',1,'QUnitConversionFamily::addConversionRule()'],['../class_q_unit_convertor.html#a433f56e02b285daabedc08d4828e2edb',1,'QUnitConvertor::addConversionRule()']]],
   ['aliases_2',['aliases',['../class_q_alias_dictionary.html#adb855bbdb04f3a466df595da7626922a',1,'QAliasDictionary']]]
 ];
